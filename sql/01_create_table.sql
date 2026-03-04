d60807246d61f9b4c307061d718020825400a123
Aaron Binder <aaronbinder81@gmail.com>
2026-03-04 11:43:05 -0600

    Create bitcoin_data table with relevant columns

---
+++ sql/01_create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bitcoin_data (
+    trans_date DATE,
+    price_usd NUMERIC,
+    transactions INTEGER,
+    median_transaction_fee NUMERIC,
+    transaction_fees NUMERIC,
+    market_cap NUMERIC,
+    code_size NUMERIC
+);
